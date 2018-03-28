fd8d4d83b2e9b9ec46dd61068682318ded668b06
Loicdelanghe <36474008+Loicdelanghe@users.noreply.github.com>
2018-03-28 11:30:04 +0200

    Create sampledata.py

---
+++ bokeh/sampledata.py
@@ -0,0 +1,20 @@
+Claus_jaren=[1950,1952,1956,1962,1963,1971,1972,1972,1977,1978,1983,1988,1994,1998]
+CLaus_roman=["De Metsiers","De hondsdagen","De koele minnaar","De Verwondering","Omtrent Deedee",
+"Schola nostra","Schaamte","Het jaar van de kreeft","Jessica","Het verlangen","Een zachte vernieling",
+"Belladonna","De geruchten","Onvoltooid verleden"]
+Claus_getal=[20,30,25,35,28,32,37,45,60,80,82,85,83,92]
+
+Elsschot_jaren=[1913,1921,1921,1924,1933,1934,1937,1938,1940,1942,1946]
+Elsschot_roman=["Villa des Roses","Een Ontgoocheling","De Verlossing","Lijmen","Kaas","Tsjip","Pensioen","Lijmen/Het been","De Leeuwentemmer","Het tankschip","Het dwaallicht"]
+Elsschot_getal=[22,38,25,24,28,32,37,45,29,33,41]
+
+Claus_leeftijd=[]
+for item in Claus_jaren:
+    leeftijd=item-1929
+    Claus_leeftijd.append(leeftijd)
+
+
+Elsschot_leeftijd=[]
+for item in Elsschot_jaren:
+    leeftijd=item-1882
+    Elsschot_leeftijd.append(leeftijd)
