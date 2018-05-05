7f150fa69d748eaabe0f07456dbab40fd1de387d
Loicdelanghe <36474008+Loicdelanghe@users.noreply.github.com>
2018-05-05 11:55:59 +0200

    Update bokehsamplev2.py
    Gaussian processor, style improvements, changed var names, added comments to explain certain functions

--- bokeh/bokehsamplev2.py
+++ bokeh/bokehsamplev2.py
@@ -10,13 +10,15 @@ from bokeh.models import CheckboxGroup, CustomJS
 import numpy as np
 import pandas as pd
 from bokeh.palettes import Dark2_5 as palette
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.gaussian_process.kernels import RBF, WhiteKernel
 
 
 
 
-output_file("sampleClausElsschot.html")
+output_file("Timeline Comparison.html")
 
-
+#set of functions to extract the data from csv files
 df = pd.read_csv('Claus_data2.csv')
 df2 = pd.read_csv('Elsschot_data2.csv')
 
@@ -63,6 +65,7 @@ for item in datasets:
     doc_data.append(extract_doc(item))
 
 
+#functions used to plot trendlines and data
 
 def trendlineslinear(Scores,age):
     Scores=np.asarray(Scores)
@@ -85,55 +88,92 @@ def trendlineslinear(Scores,age):
     return Trendline
 
 
-def datatables(titels,scores):
+def gauss_trend(Score,age):
+
+    L=np.array(age)
+    X = L.reshape(-1,1)
+    y = Score
+
+    kernel = 1.0 * RBF(length_scale=100.0, length_scale_bounds=(1e-2, 1e3)) \
+        + WhiteKernel(noise_level=1, noise_level_bounds=(1e-10, 1e+1))
+    gp = GaussianProcessRegressor(kernel=kernel,
+                              alpha=0.0).fit(X, y)
+    X_ = np.linspace(20, 80)
+    y_mean, y_cov = gp.predict(X_[:, np.newaxis], return_cov=True)
+
+    sourceline = ColumnDataSource(data=dict(
+        x=X_,
+        y=y_mean,
+        ))
+
+    Trendline=p5.line('x','y',line_width=3,source=sourceline,color="green",legend="Trendline")
+    return Trendline
+
+
+
+
+def datatables(doc_name,scores):
     data = dict(
-            roman=titels,
+            Doc_name=doc_name,
             score=scores,
         )
     source_tabel1 = ColumnDataSource(data)
 
 
     columns = [
-            TableColumn(field="roman", title="Roman"),
+            TableColumn(field="Doc_name", title="Doc_name"),
             TableColumn(field="score", title="Score"),
         ]
     data_table = DataTable(source=source_tabel1, columns=columns, width=300, height=400)
     return data_table
 
 
-def data_plot_1(leeftijd,scores,titels,authors,a):
+def data_plot_1(age,scores,doc_name,authors,a):
     sourceline = ColumnDataSource(data=dict(
-        x=leeftijd,
+        x=age,
         y=scores,
-        Titel=titels,
+        Titel=doc_name,
     ))
 
 
-    datapunten=p1.circle('x', 'y', size=20, name=authors[a], source=sourceline,color=colors[a],legend=authors[a])
-    return datapunten
+    data_circle=p1.circle('x', 'y', size=20, name=authors[a], source=sourceline,color=colors[a],legend=authors[a])
+    return data_circle
 
 
-def data_plot_2(leeftijd,scores,titels,authors,a):
+def data_plot_2(age,scores,doc_name,authors,a):
     sourceline = ColumnDataSource(data=dict(
-        x=leeftijd,
+        x=age,
         y=scores,
-        Titel=titels,
+        Titel=doc_name,
     ))
 
 
-    datapunten=p2.circle('x', 'y', size=20, name=authors[a], source=sourceline,color=colors[a],legend=authors[a])
-    return datapunten
+    data_circle=p2.circle('x', 'y', size=20, name=authors[a], source=sourceline,color=colors[a],legend=authors[a])
+    return data_circle
+
+def data_plot_3(age,scores,doc_name,authors,a):
+    sourceline = ColumnDataSource(data=dict(
+        x=age,
+        y=scores,
+        Titel=doc_name,
+    ))
+
+
+    data_circle=p5.circle('x', 'y', size=20, name=authors[a], source=sourceline,color=colors[a],legend=authors[a])
+    return data_circle
 
 
 
-def msq(Scores,leeftijd,jaartallen,a):
+#functions used to calculate additional metrics for the data analysis
+
+def msq(Scores,age,a):
     Scores=np.asarray(Scores[a])
-    jaartallen=np.asarray(leeftijd[a])
+    new_age=np.asarray(age[a])
     Scores= Scores.reshape(-1,1)
-    jaartallen = jaartallen.reshape(-1,1)
+    new_age = new_age.reshape(-1,1)
     regr = linear_model.LinearRegression()
-    regr.fit(jaartallen, Scores)
-    prediction = regr.predict(jaartallen)
+    regr.fit(new_age, Scores)
+    prediction = regr.predict(new_age)
     prediction=prediction.tolist()
     msq=mean_squared_error(Scores, prediction)
     return msq
@@ -154,12 +194,26 @@ def info(authors,Score_data,a,predicted_vals):
     width=500, height=100)
     return pre2
 
+
+
 if __name__ == '__main__':
+
+    #get colors from the Bokeh palet depending on the number of authors
     colors=[]
     [colors.append(x) for x in palette]
     colors=colors[:len(authors)]
 
+    index_authors = []
+    [index_authors.append(authors.index(item)) for item in authors]
 
+    #creates a set of empty lists that will be used to store additional metrics
+    #(list format to facilitate organising the layout of the plots)
+    empty=[]
+    predicted_vals=[]
+    infos=[]
+    mean_score=[]
+
+    #define toolset and hovertool
     tools = "pan,wheel_zoom,box_zoom,reset,save,tap".split(',')
     hover = HoverTool(tooltips=[
         ("Titel","@Titel"),
@@ -168,25 +222,26 @@ if __name__ == '__main__':
     tools.append(hover)
 
 
-
+    #TAB1: Main plot, plots a timeline of all documents (x-axis: years, y-axis: scores)
     p1 = figure(plot_width=1000, plot_height=400, tools=tools,
-               title="Romans Claus/Elsschot")
+               title="Document timeline")
+    [data_plot_1(year_data[a],Score_data[a],doc_data[a],authors,a) for a in index_authors]
     p1.background_fill_color = "#dddddd"
     p1.yaxis.axis_label = "Markers 1000/words"
     p1.yaxis.axis_label_standoff = 20
-    for item in authors:
-        a=authors.index(item)
-        data_plot_1(year_data[a],Score_data[a],doc_data[a],authors,a)
-
     p1.legend.location = "top_left"
     p1.legend.click_policy="hide"
 
-
+    #TAB1: subplots used to display additional information on the documents (e.g mean and mean sentence length)
+    [mean_score.append(sum(Score_data[a])/len(Score_data[a])) for a in index_authors]
 
     p3 = figure(x_range=authors,plot_width=500, plot_height=400, title="Mean",
            toolbar_location=None, tools="")
+    p3.vbar(x=authors, top=mean_score, width=0.3,color=colors)
     p3.background_fill_color = "#dddddd"
-    p3.vbar(x=authors, top=[30,32], width=0.3,color=colors)
+    p3.yaxis.axis_label = "Mean score"
+    p3.yaxis.axis_label_standoff = 20
+    p3.xaxis.axis_label = "Author"
 
     p4 = figure(x_range=authors,plot_width=500, plot_height=400, title="Mean sentence length (words)",
            toolbar_location=None, tools="")
@@ -198,42 +253,48 @@ if __name__ == '__main__':
 
 
 
+    #TAB2: plots linear trendline for the data (x-axis: author age, y-axis: document score)
+    p2 = figure(plot_width=1000, plot_height=400, tools="pan,wheel_zoom,box_zoom,reset,save".split(','),title="Linear regression analysis")
 
-    p2 = figure(plot_width=1000, plot_height=400, tools="pan,wheel_zoom,box_zoom,reset,save".split(','),title="Leeftijd")
+    [data_plot_2(age_data[a],Score_data[a],doc_data[a],authors,a) for a in index_authors]
+    [trendlineslinear(Score_data[a],age_data[a]) for a in index_authors]
+
     p2.background_fill_color = "#dddddd"
     p2.yaxis.axis_label = "markers 1000/words"
     p2.yaxis.axis_label_standoff = 20
-    p2.xaxis.axis_label = "Leeftijd"
-    for item in authors:
-        a=authors.index(item)
-        data_plot_2(age_data[a],Score_data[a],doc_data[a],authors,a)
-    for item in authors:
-        a=authors.index(item)
-        trendlineslinear(Score_data[a],age_data[a])
-    empty=[]
-    for item in authors:
-        n=authors.index(item)
-        empty.append(datatables(doc_data[n],Score_data[n]))
-    predicted_vals=[]
-    for item in authors:
-        a=authors.index(item)
-        predicted_vals.append(msq(Score_data,age_data,year_data,a))
-    infos=[]
-    for item in authors:
-        a=authors.index(item)
-        infos.append(info(authors,Score_data,a,predicted_vals))
+    p2.xaxis.axis_label = "age"
     p2.legend.location = "top_left"
     p2.legend.click_policy="hide"
 
 
 
+    #TAB2: plots gaussian trendline for the data (x-axis: author age, y-axis: document score)
+    p5 = figure(plot_width=1000, plot_height=400, tools="pan,wheel_zoom,box_zoom,reset,save".split(','),title="Gaussian regression analysis")
 
+    [data_plot_3(age_data[a],Score_data[a],doc_data[a],authors,a) for a in index_authors]
+    [gauss_trend(Score_data[a],age_data[a]) for a in index_authors]
+
+    p5.background_fill_color = "#dddddd"
+    p5.yaxis.axis_label = "markers 1000/words"
+    p5.yaxis.axis_label_standoff = 20
+    p5.xaxis.axis_label = "age"
+    p5.legend.location = "top_left"
+    p5.legend.click_policy="hide"
+
+
+
+    #constructs data tables and additional metrics for each author that can be displayed
+    [empty.append(datatables(doc_data[a],Score_data[a])) for a in index_authors]
+    [predicted_vals.append(msq(Score_data,age_data,a)) for a in index_authors]
+    [infos.append(info(authors,Score_data,a,predicted_vals)) for a in index_authors]
+
+    #organize tab and plot layout
     layout = row(column(row(p1),row(p3,p4)),row(widgetbox(empty)))
-    layout2=row(p2,widgetbox(infos))
+    layout2=column(row(p2,widgetbox(infos)),p5)
     curdoc().add_root(layout)
     curdoc().add_root(layout2)
-    tab2 = Panel(child=layout2, title="Leeftijd")
-    tab1 = Panel(child=layout, title="Tijd")
+    tab2 = Panel(child=layout2, title="age")
+    tab1 = Panel(child=layout, title="Timeline")
 
 
     tabs = Tabs(tabs=[ tab1, tab2 ])
